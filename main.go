00fe0a228818a4fc16e0d50ab87c48d7b5a05f5e
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-10-05 14:56:42 -0400

    Move functions to image.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"math/rand"
 	"net/http"
 	"strconv"
 )
@@ -14,31 +13,6 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/image", imageHandler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func createSequence(notes int, subdivision int, timeSignature int) []bool {
-	length := subdivision * timeSignature
-	options := createOptions(length)
-	result := make([]bool, length)
-
-	for notes > 0 {
-		i := rand.Intn(len(options))                    // Choose index for get option list.
-		choice := options[i]                            // Choose option.
-		result[choice] = true                           // Change the result's index.
-		options = append(options[:i], options[i+1:]...) // Remove option.
-		notes--
-	}
-
-	return result
-}
-
-// Creates a sequence of numbers from zero to length exclusive.
-func createOptions(length int) []int {
-	options := make([]int, length)
-	for i := range options {
-		options[i] = i
-	}
-	return options
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
