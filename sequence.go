ff38f7d32ea242ccf9f3b5d98e42fafba38f28a7
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-10-06 17:59:59 -0400

    Add comments for ease of understanding.

--- sequence.go
+++ sequence.go
@@ -16,7 +16,7 @@ func createSequence(notes int, subdivision int, timeSignature int) []bool {
 		}
 		i := rand.Intn(len(options))                    // Choose index for get option list.
 		choice := options[i]                            // Choose option.
-		result[choice] = true                           // Change the result's index.
+		result[choice] = true                           // Assign choice
 		options = append(options[:i], options[i+1:]...) // Remove option.
 		notes--
 	}
@@ -42,18 +42,17 @@ func createAutoComplexSeq(notes int, timeSignature int) [][]bool {
 	options := createComplexOptions(result)
 
 	for notes > 0 {
-
 		if len(options) == 0 {
 			break
 		}
-		c1 := rand.Intn(len(options))
-		i := options[c1].index
-		c2 := rand.Intn(len(options[c1].options))
-		j := options[c1].options[c2]
-		result[i][j] = true
+		c1 := rand.Intn(len(options))             // Choose index for the first choice
+		x := options[c1].index                    // Grab its value
+		c2 := rand.Intn(len(options[c1].options)) // Choose with element in the array
+		y := options[c1].options[c2]              // Grab the element's value
+		result[x][y] = true                       // Assign choice
 
-		options[c1].options = append(options[c1].options[:c2], options[c1].options[c2+1:]...)
-		if len(options[c1].options) == 0 {
+		options[c1].options = append(options[c1].options[:c2], options[c1].options[c2+1:]...) // Remove option from the array
+		if len(options[c1].options) == 0 {                                                    // If array is empty, remove from options
 			options = append(options[:c1], options[c1+1:]...)
 		}
 
